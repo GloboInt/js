1df03da41c49c18646d02264c62fd3d72472c11d
Grant <gsr5150@gmail.com>
2018-04-06 15:29:34 -0600

    correct spelling error in old-main.js

--- scripts/old-main.js
+++ scripts/old-main.js
@@ -3,7 +3,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() 
 {
     var mySrc = myImage.getAttribute('src');
-    if(myScr === 'images/firefox-icon.png') 
+    if(mySrc === 'images/firefox-icon.png') 
     {
     myImage.setAttribute ('src','images/firefox2.jpg');
 } else 
